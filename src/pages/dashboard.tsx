707b097b05df3d3e24df1a403b55a4ddd6fa8cb7
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-02 11:28:32 +0100

    merge from main

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box } from "@chakra-ui/core"
+
+const Dashboard = () => {
+  
+
+  return (
+      <Box display='flex' w='100vw'>
+          <Box bg='#fff7f3' minHeight='100vh' marginLeft='309px' padding='10vh 5%' w='70%'>
+
+    </Box>
+      </Box>
+    
+  )
+}
+
+export default Dashboard
