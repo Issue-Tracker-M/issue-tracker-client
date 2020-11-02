a4a318d2c7d753d62f75f03d13ff01f098593c5a
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-02 12:41:20 +0100

    navbar layout

--- src/__tests__/pages/Login.test..tsx
+++ src/__tests__/pages/Login.test..tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React from 'react'
 
-
-describe("<LoginForm />", () => {
-  test("should display a blank login form, with remember me checked by default", async () => {
+describe('<LoginForm />', () => {
+  test('should display a blank login form, with remember me checked by default', async () => {
     // ???
-  });
-});
+  })
+})
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Box } from "@chakra-ui/core"
+import { Box } from '@chakra-ui/core'
+import NavBar from '../components/NavBar'
 
 const Dashboard = () => {
-  
-
   return (
-      <Box display='flex' w='100vw'>
-          <Box bg='#fff7f3' minHeight='100vh' marginLeft='309px' padding='10vh 5%' w='70%'>
-
+    <Box display="flex" w="100vw">
+      <NavBar />
+      <Box
+        // bg="#fff7f3"
+        minHeight="100vh"
+        marginLeft="309px"
+        padding="10vh 5%"
+        w="100%"
+      ></Box>
     </Box>
-      </Box>
-    
   )
 }
 
