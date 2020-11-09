6ba02582deb54000ffb08c5cfac96c52ca961c42
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-11-09 17:25:10 +0500

    Reset password page

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Dashboard from './pages/Dashboard'
 import PageNotFound from './pages/PageNotFound'
 import ForgotPassword from './pages/ForgotPassword'
 import ConfirmEmail from './pages/ConfirmEmail'
+import ResetPassword from './pages/ResetPassword'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/signup" component={Signup} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/forgot_password" component={ForgotPassword} />
+        <Route path="/reset/:token" component={ResetPassword} />
         <Route path="/confirm/:token" component={ConfirmEmail} />
         <Route component={PageNotFound} />
       </Switch>
