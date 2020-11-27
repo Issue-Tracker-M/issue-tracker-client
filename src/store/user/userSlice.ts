3b5b67b4f8e65f8b2aac45169854028e10b5a3b4
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-11-27 17:09:09 +0500

    user slice using createAsync thunk

--- src/store/user/userSlice.ts
+++ src/store/user/userSlice.ts
@@ -1,6 +1,5 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Axios from 'axios'
-import { AppThunk } from '..'
 import { baseUrl } from '../../config'
 import { setToken } from '../../helpers'
 import { loginCredentials, succesfullAuthObject, User } from './types'
@@ -13,24 +12,54 @@ const initialState: User = {
   first_name: '',
   last_name: ''
 }
+// This creates an async action creator which later can be used like regular action
+export const authenticate = createAsyncThunk(
+  'user/authenticate',
+  async (credentials: loginCredentials) => {
+    const res = await Axios.post<succesfullAuthObject>(
+      `${baseUrl}/auth/login`,
+      credentials
+    )
+    setToken(res.data.token)
+    return res.data.user
+  }
+)
+
+export const confirmEmail = createAsyncThunk(
+  'user/confirmEmail',
+  async (token: string) => {
+    const res = await Axios.post<succesfullAuthObject>(
+      `${baseUrl}/auth/confirm_email`,
+      {
+        token
+      }
+    )
+    setToken(res.data.token)
+    return res.data.user
+  }
+)
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    // authenticateUser is what is called a case reducer
-    // It can either return the new state or mutate the given state and return nothing.
-    // If it returns nothing the inner implementation makes sure that redux gets a properly updated immutable piece of state
-    authenticateUserSuccess(state, action: PayloadAction<User>) {
-      // Here i'm choosing to return the new User state because the payload is supposed to contain the whole user object.
-      return { ...state, ...action.payload }
-    },
-    // this one gets dispatched when login errors out, no need to update state so it can be an empty function
-    authenticateUserFailed() {},
     updateUser(state, action: PayloadAction<Partial<User>>) {
       // this one just merges whatever updates you want into the user state
       return { ...state, ...action.payload }
     }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(authenticate.pending, (state, action) => {
+      // This one is passed as an example, if you don't want to do anything with the dispatched action you can just leave it out of the reducer
+    })
+    builder.addCase(authenticate.fulfilled, (state, action) => {
+      // here, because i'm expecting the payload to have a whole new user object, i'm returning that as the new state
+      return action.payload
+    })
+    // I'm not including authenticate.rejected, because it doesn't affect the state in any way.
+    builder.addCase(confirmEmail.fulfilled, (state, action) => {
+      return action.payload
+    })
   }
 })
 /* returns an object that look like this: 
@@ -48,35 +77,6 @@ const userSlice = createSlice({
     updateUser: (state, action) => newState,
   }
 } */
-export const {
-  authenticateUserSuccess,
-  authenticateUserFailed,
-  updateUser
-} = userSlice.actions
-
-export const authenticateUser = (
-  credentials: loginCredentials
-): AppThunk => async (dispatch) => {
-  // Here I'm choosing not to wrap the promise into trycatch so that the form where it is used can handle and display the errors.
-  const res = await Axios.post<succesfullAuthObject>(
-    `${baseUrl}/auth/login`,
-    credentials
-  )
-  setToken(res.data.token)
-  dispatch(authenticateUserSuccess(res.data.user))
-}
-
-export const confirmUserEmail = (token: string): AppThunk => async (
-  dispatch
-) => {
-  const res = await Axios.post<succesfullAuthObject>(
-    `${baseUrl}/auth/confirm_email`,
-    {
-      token
-    }
-  )
-  setToken(res.data.token)
-  dispatch(authenticateUserSuccess(res.data.user))
-}
+export const { updateUser } = userSlice.actions
 
 export default userSlice.reducer
