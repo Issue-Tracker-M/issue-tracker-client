45a0ca7862a26a9aa905aeda0a3013ee23c940f6
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-12-01 11:32:53 +0100

    feat: create workspace modal

---
+++ src/components/Modals/createWorkspaceModal.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import {
+  Modal,
+  Input,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  FormControl,
+  FormErrorMessage,
+  ModalBody,
+  ModalCloseButton,
+  Button
+} from '@chakra-ui/react'
+import * as yup from 'yup'
+import { Formik, Form, Field } from 'formik'
+
+interface createWorkspaceModalProps {
+    isOpen: boolean
+    onClose(): any
+}
+
+interface createWorkspaceObject {
+    name: string
+}
+
+const CreateWorkspaceModal = ({
+  isOpen,
+  onClose
+}: createWorkspaceModalProps) => {
+  const validationSchema = yup.object().shape({
+    name: yup.string().label('name').required()
+    // labels: yup.array().label('labels').required()
+  })
+
+  const initialValues: createWorkspaceObject = {
+    name: ''
+    // labels: []
+  }
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add WorkSpace</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values, actions) => {
+              const payload = { name: values.name, labels: [] }
+            //   createWorkspace(payload)
+              onClose()
+            }}
+            validationSchema={validationSchema}
+          >
+            <Form
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                height: '100%'
+              }}
+            >
+              <label htmlFor="name">
+                {/* Name{' '} */}
+                <Field name="name">
+                  {({ field, form }: any) => (
+                    <FormControl
+                      isInvalid={form.errors.name && form.touched.name}
+                    >
+                      <Input
+                        {...field}
+                        id="name"
+                        size="md"
+                        variant="outline"
+                        placeholder="Name"
+                      />
+                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+              </label>
+
+              <Button mt={4} variantColor="teal" type="submit">
+                Create Workspace
+              </Button>
+            </Form>
+          </Formik>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default CreateWorkspaceModal
