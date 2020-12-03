66a1f75d4767e8d51c3313bd3c23f788e088c706
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-12-03 07:46:00 +0500

    redux root state default to displays app state

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom/extend-expect'
+import { RootState } from './store/rootReducer'
+
+declare module 'react-redux' {
+  export interface DefaultRootState extends RootState {}
+}
